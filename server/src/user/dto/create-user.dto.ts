39adc67d6c91d5711498ca998eae6f35454c9b65
EvgenDev97 <evgendev2022@gmail.com>
2024-07-07 18:24:02 +0300

    create user registration

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import {IsEmail, Min, MinLength} from "class-validator";
+
+export class CreateUserDto {
+
+    @IsEmail()
+    email:string;
+
+    @MinLength(6, {message:" password length must be at least 6 characters"})
+    password:string;
+}
