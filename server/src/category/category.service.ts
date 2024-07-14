4d8742733a83cd12c11f77c16cad75325c865be2
EvgenDev97 <evgendev2022@gmail.com>
2024-07-14 19:29:20 +0300

    category.service.ts error

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,15 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import {Repository} from "typeorm";
+import {Category} from "./entities/category.entity";
+import {InjectRepository} from "@nestjs/typeorm";
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+
+  constructor(@InjectRepository(Category)
+              private readonly categoryRepository: Repository<Category>) {
+  }
+   async create(createCategoryDto: CreateCategoryDto, id:number) {
+      //findBy = SELECT * FROM category WHERE title = "title"
+    const isExist = await this.categoryRepository.findBy({
+        //ERROR
+        title:createCategoryDto.title,
+        user:{id:id}
+    })
+    if(isExist.length) throw new BadRequestException('This category already exists');
+
+    const newCategory = {
+      title:createCategoryDto.title,
+      user:{
+        id
+      }
+    }
+
+    return this.categoryRepository.save(newCategory);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll(id:number) {
+    return await  this.categoryRepository.find({
+        //ERROR
+        where:{
+            user:{id:id}
+        }
+        // relations:{
+        //     transaction:true
+        // }
+    })
   }
 
   findOne(id: number) {
