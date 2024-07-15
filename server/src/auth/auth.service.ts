39aaaaadf78a6ced387196d6d00b00e417d666f3
unknown <evgendev2022@gmail.com>
2024-07-15 15:53:55 +0300

    login bug

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
 import {UserService} from "../user/user.service";
 import * as argon2 from "argon2";
 import {JwtService} from "@nestjs/jwt";
@@ -7,7 +7,7 @@ import {IUser} from "../types/types";
 @Injectable()
 export class AuthService {
   constructor( private userService: UserService, private jwtService: JwtService ) {}
-
+ //login
   async validateUser(email: string, password: string) {
     const user = await this.userService.findOne(email);
     const passwordIsMatch = await argon2.verify(user.password, password );
@@ -18,11 +18,20 @@ export class AuthService {
   }
 
   async login(user: IUser) {
-    const {id, email} = user
-    return {
-      id,
-      email,
-      token:this.jwtService.sign({id:user.id, email:user.email}),
-    };
+    try{
+      const {id, email} = user
+      return {
+        id,
+        email,
+        token:this.jwtService.sign({id:user.id, email:user.email}),
+      };
+    }catch (e){
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'This is a custom message',
+      }, HttpStatus.FORBIDDEN, {
+        cause: e
+      });
+    }
   }
 }
