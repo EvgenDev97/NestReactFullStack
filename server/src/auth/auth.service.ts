f5865a6c75eb4bef1b63f955cd2538843bae8436
unknown <evgendev2022@gmail.com>
2024-07-17 16:50:09 +0300

    todo

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {UserService} from "../user/user.service";
 import * as argon2 from "argon2";
 import {JwtService} from "@nestjs/jwt";
@@ -6,28 +6,30 @@ import {IUser} from "../types/types";
 
 @Injectable()
 export class AuthService {
-  constructor( private userService: UserService, private jwtService: JwtService ) {}
- //login
-  async validateUser(email: string, password: string) {
-    const user = await this.userService.findOne(email);
-    let passwordIsMatch
-    if(user){
-      passwordIsMatch = await argon2.verify(user.password, password);
-    }else{
-      throw new UnauthorizedException("the email was not match");
+    constructor(private userService: UserService, private jwtService: JwtService) {
     }
-    if (user && passwordIsMatch) {
-      return user;
-    }
-    throw new UnauthorizedException('Invalid email or password ');
-  }
 
-  async login(user: IUser) {
-    const {id, email} = user
-    return {
-      id,
-      email,
-      token:this.jwtService.sign({id:user.id, email:user.email}),
-    };
-  }
+    //login
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findOne(email);
+        let passwordIsMatch: boolean
+        if (user) {
+            passwordIsMatch = await argon2.verify(user.password, password);
+        } else {
+            throw new UnauthorizedException("the email was not match");
+        }
+        if (user && passwordIsMatch) {
+            return user;
+        }
+        throw new UnauthorizedException('Invalid email or password ');
+    }
+
+    async login(user: IUser) {
+        const {id, email} = user
+        return {
+            id,
+            email,
+            token: this.jwtService.sign({id: user.id, email: user.email}),
+        };
+    }
 }
---
+++ the learning process/registration and auth.md
@@ -0,0 +1,37 @@
+1 - create-user.dto.ts - описывает входящие параметры, service.ts принимает в себя dto
+    nest overview - pipes - class Validator
+2 - user.module - подключить user.entity - imports: [TypeOrmModule.forFeature([User])
+3 - user.controller
+4- user.service metod create
+5 - пароль необходимо хешировать
+
+
+6 - Passport (authentication) nest.js recipes passport
+7- user.service findOne
+8 - user.module export:[UserService],
+9 - auth.service validateUser function,
+10 -auth.module imports:[UserModule],
+11 - auth/local.strategy.ts copy code from docs
+12 - в auth.module import passport module and local strategy 
+13 - в auth.controller добавить @UseGuard - запрос отправляется по роуту api/auth/login, прежде чем выполнится запрос
+    выполнится, authGuard сделай проверку .Гуард заходит В local.strategy функция validateUser, который идет в auth.service функция validateUser
+14 - создать папку guards/ local-auth.guard.ts - создать отдельный новый гуард который экстендится от AuthGuard из @nestjs/passport 
+15 - передать в useGuard передать класс созданного гуарда
+/////
+17 - JWT
+18- auth.service constructor (jwtService:JwtService)
+19 - function login - при логине нужно возвращать токен, а токен необходимо формировать на основе чего либо, в данном случе id  и  email
+    this.jwtService.sign()
+    jwt хранится в .env - случайная константа
+20- в auth.module подключить jwt.module
+    JwtModule.registerAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        // token lifetime
+        signOptions: {expiresIn: "30d"}
+        }),
+    inject: [ConfigService],
+    })
+
+21 - jwt.strategy.ts 
